0d9f771e3c98f9a1509d4351c69cf0c3de09a27d
Joyer Huang <collger@gmail.com>
2026-04-06 00:44:52 +0800

    Revert "修复窗口渲染：builtin shader 替换 DXVK blit pass + swapchain 修复"
    This reverts commit 7b33cd0c7da784043e1d9f9525b2896200af90b9.

--- host/src/vn_decoder.cpp
+++ host/src/vn_decoder.cpp
@@ -3,69 +3,6 @@
 #include <fstream>
 #include <algorithm>
 
-// Built-in simple triangle shaders for use when DXVK's blit shader can't work
-// (DXVK blits from internal images we don't have — replace with direct triangle)
-// Vertex: hardcoded triangle via gl_VertexIndex
-static const uint32_t s_builtinVert[] = {
-    0x07230203,0x00010000,0x000d000a,0x00000028,0x00000000,0x00020011,0x00000001,0x0006000b,
-    0x00000001,0x4c534c47,0x6474732e,0x3035342e,0x00000000,0x0003000e,0x00000000,0x00000001,
-    0x0007000f,0x00000000,0x00000004,0x6e69616d,0x00000000,0x00000019,0x0000001d,0x00030003,
-    0x00000002,0x000001c2,0x000a0004,0x475f4c47,0x4c474f4f,0x70635f45,0x74735f70,0x5f656c79,
-    0x656e696c,0x7269645f,0x69746365,0x00006576,0x00080004,0x475f4c47,0x4c474f4f,0x6e695f45,
-    0x64756c63,0x69645f65,0x74636572,0x00657669,0x00040005,0x00000004,0x6e69616d,0x00000000,
-    0x00050005,0x0000000c,0x69736f70,0x6e6f6974,0x00000073,0x00060005,0x00000017,0x505f6c67,
-    0x65567265,0x78657472,0x00000000,0x00060006,0x00000017,0x00000000,0x505f6c67,0x7469736f,
-    0x006e6f69,0x00070006,0x00000017,0x00000001,0x505f6c67,0x746e696f,0x657a6953,0x00000000,
-    0x00070006,0x00000017,0x00000002,0x435f6c67,0x4470696c,0x61747369,0x0065636e,0x00070006,
-    0x00000017,0x00000003,0x435f6c67,0x446c6c75,0x61747369,0x0065636e,0x00030005,0x00000019,
-    0x00000000,0x00060005,0x0000001d,0x565f6c67,0x65747265,0x646e4978,0x00007865,0x00050048,
-    0x00000017,0x00000000,0x0000000b,0x00000000,0x00050048,0x00000017,0x00000001,0x0000000b,
-    0x00000001,0x00050048,0x00000017,0x00000002,0x0000000b,0x00000003,0x00050048,0x00000017,
-    0x00000003,0x0000000b,0x00000004,0x00030047,0x00000017,0x00000002,0x00040047,0x0000001d,
-    0x0000000b,0x0000002a,0x00020013,0x00000002,0x00030021,0x00000003,0x00000002,0x00030016,
-    0x00000006,0x00000020,0x00040017,0x00000007,0x00000006,0x00000002,0x00040015,0x00000008,
-    0x00000020,0x00000000,0x0004002b,0x00000008,0x00000009,0x00000003,0x0004001c,0x0000000a,
-    0x00000007,0x00000009,0x00040020,0x0000000b,0x00000006,0x0000000a,0x0004003b,0x0000000b,
-    0x0000000c,0x00000006,0x0004002b,0x00000006,0x0000000d,0x00000000,0x0004002b,0x00000006,
-    0x0000000e,0xbf000000,0x0005002c,0x00000007,0x0000000f,0x0000000d,0x0000000e,0x0004002b,
-    0x00000006,0x00000010,0x3f000000,0x0005002c,0x00000007,0x00000011,0x00000010,0x00000010,
-    0x0005002c,0x00000007,0x00000012,0x0000000e,0x00000010,0x0006002c,0x0000000a,0x00000013,
-    0x0000000f,0x00000011,0x00000012,0x00040017,0x00000014,0x00000006,0x00000004,0x0004002b,
-    0x00000008,0x00000015,0x00000001,0x0004001c,0x00000016,0x00000006,0x00000015,0x0006001e,
-    0x00000017,0x00000014,0x00000006,0x00000016,0x00000016,0x00040020,0x00000018,0x00000003,
-    0x00000017,0x0004003b,0x00000018,0x00000019,0x00000003,0x00040015,0x0000001a,0x00000020,
-    0x00000001,0x0004002b,0x0000001a,0x0000001b,0x00000000,0x00040020,0x0000001c,0x00000001,
-    0x0000001a,0x0004003b,0x0000001c,0x0000001d,0x00000001,0x00040020,0x0000001f,0x00000006,
-    0x00000007,0x0004002b,0x00000006,0x00000022,0x3f800000,0x00040020,0x00000026,0x00000003,
-    0x00000014,0x00050036,0x00000002,0x00000004,0x00000000,0x00000003,0x000200f8,0x00000005,
-    0x0003003e,0x0000000c,0x00000013,0x0004003d,0x0000001a,0x0000001e,0x0000001d,0x00050041,
-    0x0000001f,0x00000020,0x0000000c,0x0000001e,0x0004003d,0x00000007,0x00000021,0x00000020,
-    0x00050051,0x00000006,0x00000023,0x00000021,0x00000000,0x00050051,0x00000006,0x00000024,
-    0x00000021,0x00000001,0x00070050,0x00000014,0x00000025,0x00000023,0x00000024,0x0000000d,
-    0x00000022,0x00050041,0x00000026,0x00000027,0x00000019,0x0000001b,0x0003003e,0x00000027,
-    0x00000025,0x000100fd,0x00010038,
-};
-// Fragment: constant orange
-static const uint32_t s_builtinFrag[] = {
-    0x07230203,0x00010000,0x000d000a,0x0000000e,0x00000000,0x00020011,0x00000001,0x0006000b,
-    0x00000001,0x4c534c47,0x6474732e,0x3035342e,0x00000000,0x0003000e,0x00000000,0x00000001,
-    0x0006000f,0x00000004,0x00000004,0x6e69616d,0x00000000,0x00000009,0x00030010,0x00000004,
-    0x00000007,0x00030003,0x00000002,0x000001c2,0x000a0004,0x475f4c47,0x4c474f4f,0x70635f45,
-    0x74735f70,0x5f656c79,0x656e696c,0x7269645f,0x69746365,0x00006576,0x00080004,0x475f4c47,
-    0x4c474f4f,0x6e695f45,0x64756c63,0x69645f65,0x74636572,0x00657669,0x00040005,0x00000004,
-    0x6e69616d,0x00000000,0x00050005,0x00000009,0x4374756f,0x726f6c6f,0x00000000,0x00040047,
-    0x00000009,0x0000001e,0x00000000,0x00020013,0x00000002,0x00030021,0x00000003,0x00000002,
-    0x00030016,0x00000006,0x00000020,0x00040017,0x00000007,0x00000006,0x00000004,0x00040020,
-    0x00000008,0x00000003,0x00000007,0x0004003b,0x00000008,0x00000009,0x00000003,0x0004002b,
-    0x00000006,0x0000000a,0x3f800000,0x0004002b,0x00000006,0x0000000b,0x3f000000,0x0004002b,
-    0x00000006,0x0000000c,0x00000000,0x0007002c,0x00000007,0x0000000d,0x0000000a,0x0000000b,
-    0x0000000c,0x0000000a,0x00050036,0x00000002,0x00000004,0x00000000,0x00000003,0x000200f8,
-    0x00000005,0x0003003e,0x00000009,0x0000000d,0x000100fd,0x00010038,
-};
-static VkShaderModule s_builtinVertMod = VK_NULL_HANDLE;
-static VkShaderModule s_builtinFragMod = VK_NULL_HANDLE;
-static VkPipelineLayout s_builtinLayout = VK_NULL_HANDLE;
-
 void VnDecoder::init(VkPhysicalDevice physDevice, VkDevice device,
                      VkQueue graphicsQueue, uint32_t graphicsFamily,
                      VkSurfaceKHR surface) {
@@ -325,38 +262,6 @@ void VnDecoder::handleCreateGraphicsPipeline(VnStreamReader& r) {
             (int)dynamicRendering, colorFmt, vpWidth, vpHeight, (void*)vertMod, (void*)fragMod);
     fflush(stderr);
 
-    // Check if this is DXVK's blit/present pipeline (has descriptor sets in layout).
-    // DXVK blits from internal images we don't have → replace with builtin triangle shader.
-    VkPipelineLayout origLayout = lookup(pipelineLayouts_, layoutId);
-    bool isBlit = dynamicRendering && (layoutId != 0) && (fragModuleId != 0);
-    // Heuristic: blit pipeline has a large fragment shader (>2000 bytes) and push constants
-    // The clear/init pipeline has a small fragment shader and empty layout
-    // For now, check if layout has descriptor sets by checking if it's not the empty layout
-    // (empty layout=25 in the dump has setLayouts=[] pushRanges=[])
-    // A proper check: the blit layout (19) has push constants, the clear layout (25) doesn't.
-    // We detect blit by looking at the pipeline layout's push constant presence.
-    // Since we can't introspect VkPipelineLayout, use shader size as proxy:
-    // shader_38 (clear frag) = 684 bytes, shader_45 (blit frag) = 15656 bytes
-    // The SPIR-V code is already loaded into the shader module, but we don't track sizes.
-    // Simpler approach: if the pipeline uses push constants (layout != empty), it's blit.
-
-    if (dynamicRendering && isBlit) {
-        // Create builtin shader modules lazily
-        if (!s_builtinVertMod) {
-            VkShaderModuleCreateInfo ci{}; ci.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-            ci.codeSize = sizeof(s_builtinVert); ci.pCode = s_builtinVert;
-            vkCreateShaderModule(device_, &ci, nullptr, &s_builtinVertMod);
-            ci.codeSize = sizeof(s_builtinFrag); ci.pCode = s_builtinFrag;
-            vkCreateShaderModule(device_, &ci, nullptr, &s_builtinFragMod);
-            VkPipelineLayoutCreateInfo pli{}; pli.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-            vkCreatePipelineLayout(device_, &pli, nullptr, &s_builtinLayout);
-        }
-        vertMod = s_builtinVertMod;
-        fragMod = s_builtinFragMod;
-        origLayout = s_builtinLayout;
-        fprintf(stderr, "[Decoder] CreatePipeline: BLIT detected, using builtin triangle shader\n");
-    }
-
     if (!vertMod || !fragMod) {
         fprintf(stderr, "[Decoder] CreatePipeline: missing shader, skipping\n");
         store(pipelines_, pipelineId, (VkPipeline)VK_NULL_HANDLE);
@@ -464,7 +369,7 @@ void VnDecoder::handleCreateGraphicsPipeline(VnStreamReader& r) {
     pInfo.pMultisampleState = &ms;
     pInfo.pColorBlendState = &cb;
     pInfo.pDynamicState = (dynamicRendering || dynState.dynamicStateCount > 0) ? &dynState : nullptr;
-    pInfo.layout = origLayout;
+    pInfo.layout = lookup(pipelineLayouts_, layoutId);
 
     if (dynamicRendering) {
         renderingInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
@@ -816,11 +721,9 @@ void VnDecoder::handleCmdPushConstants(VnStreamReader& r) {
     VkCommandBuffer cb = lookup(commandBuffers_, cbId);
     VkPipelineLayout layout = lookup(pipelineLayouts_, layoutId);
     if (!cb || !layout) return;
-    // When blit pipeline is replaced with builtin, the currently bound pipeline
-    // uses s_builtinLayout (empty). PushConstants with the DXVK layout would
-    // mismatch. Just silently skip — builtin shader doesn't use push constants.
-    // TODO: remove this when proper blit is implemented.
     vkCmdPushConstants(cb, layout, stageFlags, offset, size, data.data());
+    fprintf(stderr, "[Decoder] PushConstants: cb=%p stage=0x%x offset=%u size=%u\n",
+            (void*)cb, stageFlags, offset, size);
 }
 
 // --- Sync ---
@@ -1048,6 +951,15 @@ void VnDecoder::flushPendingPresents() {
         }
 
         VkResult vr = vkQueuePresentKHR(graphicsQueue_, &info);
+
+        static int wgcFrame = 0;
+        wgcFrame++;
+        if (wgcFrame == 5) {
+            HWND wnd = FindWindowA("VBoxGPUBridgeServer", nullptr);
+            if (!wnd) wnd = FindWindowA(nullptr, "VBox GPU Bridge - Host Server");
+            if (wnd) captureWindowToBMP(wnd, "S:/bld/vboxgpu/wgc_capture.bmp");
+        }
+
         fprintf(stderr, "[Decoder] Present FLUSH: sc=%p imgIdx=%u result=%d\n",
                 (void*)it->second.swapchain, presentIdx, (int)vr);
         fflush(stderr);
